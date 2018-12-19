d743ca312c70ce61d035dc139de2ab98469c83fc
Gianfranco Trogetto <trogettog@gmail.com>
2018-12-18 22:19:43 -0300

    Evento agrandar botones

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 var calculadora = {
 
 	init: (function(){
-		this.eventosBtn(".tecla");
+		this.eventosBtn("teclado");
 	}),
 
 //Evento para botones
 
 	eventosBtn: function(selector){
-		var selectorBtn = document.querySelectorAll(selector);
+		var selectorBtn = document.querySelectorAll('.' + selector + ' img')
 		for (var i=0; i<selectorBtn.length; i++) {
 			selectorBtn[i].onclick = this.eventoBtnCh;
+			selectorBtn[i].onmouseleave = this.eventoBtnGr;
 
 
 		}
@@ -19,6 +20,10 @@ var calculadora = {
 		calculadora.btnCh(event.target);
 	},
 
+	eventoBtnGr: function(event){
+		calculadora.btnGr(event.target);
+	},
+
 
 
 //Formato botones
@@ -26,11 +31,15 @@ var calculadora = {
 	btnCh: function(element){
 		var tecla = element.id;
 		if (tecla=='1' || tecla=='2' || tecla=='3' || tecla=='4' || tecla=='5' || tecla=='6' || tecla=='7' || tecla=='8' || tecla=='9' || tecla=='0') {
-		element.style.padding = "1.3px"
+		element.style.padding = "1.5px"
 		} else {
-			element.style.padding = "1.3px"
+			element.style.padding = "1.0px"
 		}
 	},
+
+	btnGr: function(elementoDOM){
+			elementoDOM.style.padding = '0px';
+		}
 
 };
 
